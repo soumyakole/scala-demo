c24f032cf0b0c38856b2373806954109c770c5d4
soumyabrata kole <soumyabrata.kole@icloud.com>
2019-11-17 01:29:27 +0530

    initial commit

---
+++ src/main/scala/com/github/soumyakole/datastructure/ArraySwap.scala
@@ -0,0 +1,19 @@
+package com.github.soumyakole.datastructure
+
+/**
+  * Write a loop that swaps adjacent elements of an array of integers. For example, Array(1, 2, 3, 4, 5) becomes Array(2, 1, 4, 3, 5)
+  */
+
+object ArraySwap extends App {
+
+  val a = Array(1,2,3,4,5);
+
+  val solution_1 = a.grouped(2).flatMap(_.reverse).toArray
+
+  println(solution_1.mkString(" "))
+
+  val solution_2 = for {b <- a.grouped(2); c <- b.reverse} yield c
+
+  println(solution_2.mkString(" "))
+
+}
