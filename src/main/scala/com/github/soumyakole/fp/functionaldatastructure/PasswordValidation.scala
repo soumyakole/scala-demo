56b389be8a85b239ea958b772910956a8510572f
soumyabrata kole <soumyabrata.kole@icloud.com>
2019-11-22 09:38:44 +0530

    initial commit

---
+++ src/main/scala/com/github/soumyakole/fp/functionaldatastructure/PasswordValidation.scala
@@ -0,0 +1,34 @@
+package com.github.soumyakole.fp.functionaldatastructure
+
+
+/**
+  * Each password must now have at least six characters and a mix of upper and lowercase letters.
+  * So, given a list of password strings, we need to compute a corresponding list of Booleans that will return true if the password qualifies, and otherwise false.
+  */
+
+object PasswordValidation extends App{
+  val passwords = List("oddoddboy@thedoor", "likelynew", "alwaysThinkOfMyself", "naughty")
+
+  //Length checker
+  def lengthGreaterThan(minLen: Int)(s: String) = s.length >= minLen
+  val validatePasswdLen: String => Boolean = lengthGreaterThan(6) _
+
+  //Char checker
+  val containsChar: (Char, String) => Boolean = (c: Char, s: String) => s.contains(c)
+  def containsOneOf(inChars: Seq[Char])(s: String): Boolean = inChars exists(c => containsChar(c,s))
+  val containsUpper: String => Boolean = containsOneOf('A' to 'Z') _
+  val containsLower: String => Boolean = containsOneOf('a' to 'z') _
+
+  //Final Check function
+  val checker = (pass:String) => validatePasswdLen(pass) && containsLower(pass) && containsUpper(pass)
+
+  //Using map
+  val result: Seq[(String, Boolean)] = passwords.map(x => (x, checker(x)))
+  println(result)
+  
+  //using for 
+  val result1: Seq[(String, Boolean)] = for (w <- passwords) yield (w,checker(w))
+  println(result1)
+
+
+}
